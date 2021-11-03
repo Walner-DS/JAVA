4482a167d32675bfe141b88e3f4e38d1c20965be
Walner-DS <walnerpassos.ds@gmail.com>
2021-11-03 16:01:23 -0300

    first commit

---
+++ src/ImprimirVariaveis.java
@@ -0,0 +1,19 @@
+public class ImprimirVariaveis {
+    public static void main(String[] args){
+        // Comentário em linha
+        /*
+            Comentário em bloco
+         */
+        /*
+         * Comentário para documentação do código
+         */
+
+        /* Tipos primitivos
+           int, double, flot, boolean, char, byte, short, long
+         */
+        // Declaração de variáveis
+         int idade = 57;
+         System.out.println("Declarando e imprimindo variáveis primitivas");
+         System.out.println("Variável do tipo interiro : " + idade);
+    }
+}
